373265ec9f985be8edc88b2f46dac9e2757b781e
Koo <kooroshr@hotmail.com>
2024-04-16 15:33:12 +0100

    completed 10-GET/api/users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -359,3 +359,25 @@ describe('checkArticleExists', () => {
 
 
 });
+
+describe('/api/users', () => {
+    describe('GET /api/users', () => {
+        test('GET 200 /api/users', () => {
+            return request(app)
+                .get('/api/users')
+                .expect(200)
+                .then(({ body: { users } }) => {
+                    expect(users.length).toBe(4)
+                    users.forEach((user) => {
+                        expect(typeof user.username).toBe('string')
+                        expect(typeof user.name).toBe('string')
+                        expect(typeof user.avatar_url).toBe('string')
+                    })
+
+                })
+        });
+
+
+
+    });
+});
