064c850b794399426cef2d097fd06535e2affd5a
Koo <kooroshr@hotmail.com>
2024-04-17 13:49:19 +0100

    refactored to remove redundant async column check

--- models/articles.model.js
+++ models/articles.model.js
@@ -7,6 +7,7 @@ exports.fetchArticles = (query) => {
         "sort_by",
         "order"
     ]
+    const columns = ['article_id', 'title', 'topic', 'author', 'created_at', 'votes', 'article_img_url', 'comment_count']
     let queryString = ``
     const SQLString = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, article_img_url, COUNT(comments.article_id)::INTEGER AS comment_count FROM articles LEFT JOIN comments ON comments.article_id=articles.article_id`
     const groupByString = ` GROUP BY articles.article_id`
@@ -25,32 +26,14 @@ exports.fetchArticles = (query) => {
 
 
         if (key === 'sort_by') {
-            return exports.checkColumnExists(value)
-                .then((result) => {
-                    if (result) {
-                        orderByString = ` ORDER BY articles.${value} DESC`
-                        queryString = ``
-                        return db.query(SQLString + queryString + groupByString + orderByString, positionalParams)
-                            .then(({ rows }) => {
-
-                                if (!rows.length) {
-                                    return Promise.reject({ status: 404, msg: "Not found" })
-                                }
-                                return rows
-                            })
-                    } else {
-                        return Promise.reject({ status: 404, msg: "Not found" })
-                    }
-                })
+            if (!columns.includes(value)) {
+                return Promise.reject({ status: 404, msg: "Not found" })
+            }
+            orderByString = ` ORDER BY articles.${value} DESC`
+            queryString = ``
 
         }
-
     }
-
-
-
-
-
 
     return db.query(SQLString + queryString + groupByString + orderByString, positionalParams)
         .then(({ rows }) => {
@@ -116,12 +99,30 @@ exports.updateArticleById = (article_id, update) => {
         })
 }
 
-exports.checkColumnExists = (column) => {
-    return db.query(`SELECT * 
-        FROM INFORMATION_SCHEMA.COLUMNS 
-        WHERE TABLE_NAME = N'articles'`)
-        .then(({ rows }) => {
-            const existing = rows.map((row) => row.column_name)
-            return existing.includes(column)
-        })
-}
+// exports.checkColumnExists = (column) => {
+//     return db.query(`SELECT * 
+//         FROM INFORMATION_SCHEMA.COLUMNS 
+//         WHERE TABLE_NAME = N'articles'`)
+//         .then(({ rows }) => {
+//             const existing = rows.map((row) => row.column_name)
+//             return existing.includes(column)
+//         })
+// }
+
+// return exports.checkColumnExists(value)
+//     .then((result) => {
+//         if (result) {
+//             orderByString = ` ORDER BY articles.${value} DESC`
+//             queryString = ``
+//             return db.query(SQLString + queryString + groupByString + orderByString, positionalParams)
+//                 .then(({ rows }) => {
+
+//                     if (!rows.length) {
+//                         return Promise.reject({ status: 404, msg: "Not found" })
+//                     }
+//                     return rows
+//                 })
+//         } else {
+//             return Promise.reject({ status: 404, msg: "Not found" })
+//         }
+//     })
