3e93ae0c4827b8a3ddabc5338ff585be74e1b5b9
Koo <kooroshr@hotmail.com>
2024-04-17 09:51:03 +0100

    refactored remove/deleteCommentById - was passing due to ifExists completing before SQL query *MOST* of the time, by chance

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,7 +3,7 @@ const { deleteCommentById, checkCommentExists } = require('../models/comments.mo
 
 exports.removeCommentById = (req, res, next) => {
     const { comment_id } = req.params
-    Promise.all([deleteCommentById(comment_id), checkCommentExists(comment_id)])
+   return deleteCommentById(comment_id)
         .then(() => {
             res.status(204).send()
         })
--- models/comments.model.js
+++ models/comments.model.js
@@ -2,7 +2,10 @@ const db = require('../db/connection')
 
 exports.deleteCommentById = (comment_id) => {
     return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [comment_id])
-        .then(() => {
+        .then(({ rows }) => {
+            if (!rows.length) {
+                return Promise.reject({ status: 404, msg: "Not found" })
+            }
             return
         })
 }
