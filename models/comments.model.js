448dbfb6d8b60a0465c6cc0df634fb0f03f38a29
Koo <kooroshr@hotmail.com>
2024-04-18 13:21:48 +0100

    completed 18-ADV-PATCH-/api/comments/-comment_id and associated tests

--- models/comments.model.js
+++ models/comments.model.js
@@ -10,6 +10,14 @@ exports.deleteCommentById = (comment_id) => {
         })
 }
 
+exports.updateCommentById = (comment_id, update)=>{
+    const { inc_votes } = update
+    return db.query(`UPDATE comments SET votes = votes + $2 WHERE comment_id = $1 RETURNING *;`, [comment_id, inc_votes])
+        .then(({ rows }) => {
+            return rows
+        })
+}
+
 exports.checkCommentExists = (comment_id) => {
     if (isNaN(+comment_id)) {
         return Promise.reject({ status: 400, msg: "Invalid input" })
