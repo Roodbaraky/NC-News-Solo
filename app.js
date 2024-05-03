127b93f1a69886550acfb0df39f1e0f1cf0e6647
Koo <kooroshr@hotmail.com>
2024-05-03 15:03:29 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
+
 
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors, catchAll } = require('./errors/index')
 
 app.use(express.json())
+app.use(cors());
 
 app.use('/api', apiRouter)
 
- 
+
 app.all('*', catchAll)
 app.use(handleCustomErrors)
 app.use(handlePsqlErrors)
