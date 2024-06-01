6fa6acaa1d7d5babe8a26d7f7225bb9cc46f8f1e
roodbaraky <kooroshr@hotmail.com>
2024-06-01 09:38:26 +0100

    cors workaround

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
 const apiRouter = require('./routes/api-router');
-const cors = require('cors');
+
 
 
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors, catchAll } = require('./errors/index')
 
-app.use(cors());
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:5174', 'https://nc-news-solo.onrender.com', 'https://nc-news-solo.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 app.use(express.json())
 
 app.use('/api', apiRouter)
